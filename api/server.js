0e6120212338f8ab572b8d9f02e1c001c9202274
amgowda.52 <annappagowda@blipsnip.local>
2025-11-20 15:00:23 +0530

    understanding server creation and http module

---
+++ api/server.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello, World! from the server! ${name}`;
+}
+const name = 'Vehiclete User';
+console.log(greet(name));
+//# sourceMappingURL=server.js.map
