1b6be55e0c3c9b05bd801b667c9c87fc18d0996c
Annappa Gowda <109304720+amgowda42@users.noreply.github.com>
2026-03-03 14:49:49 +0530

    Update server.ts

--- api/server.ts
+++ api/server.ts
@@ -68,16 +68,16 @@ app.all('/*splat', (req, res) => {
 app.use(errorHandler);
 
 
-mongoose.connection.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
 // mongoose.connection.once('open', () => {
 //   console.log('Connected to MongoDB');
-//   app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-//   });
 // });
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
 
 process.on('unhandledRejection', (err: unknown) => {
   if (err instanceof Error) {
