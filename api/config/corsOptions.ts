1dd23a1e2a32c532ba61f83fd63fe90b5dabeaf6
Annappa Gowda <109304720+amgowda42@users.noreply.github.com>
2026-05-10 14:21:48 +0530

    Add localhost:3000 to allowed CORS origins

--- api/config/corsOptions.ts
+++ api/config/corsOptions.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:8000',
+  'http://localhost:3000',
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
